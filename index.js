0f0dd83c2e31f6af16aeda256faf9d1d1685162d
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-06-30 16:48:09 +0300

    Проверил ответ с сервера

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.end('Hello from nodejs')
+})
+
 app.listen(3000, () => {
     console.log('Сервер запущен. Порт 3000.')
 })
