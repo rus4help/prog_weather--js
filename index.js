567a8e538b473db780eb461275353cac24cb984c
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-06-30 18:18:05 +0300

    Обработка формы закончена

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/', (req, res) => {
+    const {city} = req.body
+    console.log(city);
+    
     res.render('index')
 })
 
